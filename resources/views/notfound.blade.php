c56bc13d30e519597160c952d5bad2c96a3a3287
Bintang Diyantoro <bintangdiyantoro@gmail.com>
2018-12-04 14:45:50 +0700

    fallback

--- resources/views/notfound.blade.php
+++ resources/views/notfound.blade.php
@@ -1,7 +1,7 @@
 <h2>Page not available or maybe broken</h2>
 
-<h3>I don't know, just trying something</h3>
-<p>So try to go to another link</p>
+<h3>I don't know, just try to go to another link</h3>
+
 <!-- Dududu
 Nanana
 lalala -->
