810ec131b303343667337b342df5e2c3ee85740f
Bintang Diyantoro <bintangdiyantoro@gmail.com>
2018-12-27 16:56:46 +0700

    Role middleware & use auth middleware

--- routes/web.php
+++ routes/web.php
@@ -193,3 +193,7 @@ Route::get('/home', 'HomeController@index')->name('home');
 // });
 
 Route::get('/user', 'HomeController@user');
+
+Route::get('/admin', function(){
+    return "Hi, this is admin page";
+})->middleware(['role','auth']);
